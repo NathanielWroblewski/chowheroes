668b8181860ba5fe276117bbf70be711521e140c
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-07-01 22:37:33 -0700

    chow down!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chowheroes::Application.config.secret_key_base = '7c0d1a2563685d1004a69fe506168688270425a2422ef5b746840f14c11fd63acbf9ef04eb0f62961465c5a9ddbbceacd4baf579b5cbbd42933856cb2c4e0c2b'
