4b3fe2e438487a61a417959c54716fe533f84f25
sidney zhang <sidazhang89@gmail.com>
2013-07-02 01:25:55 -0700

    Driver code file in initiailizers to speed up development

---
+++ config/initializers/drivers.rb
@@ -0,0 +1,7 @@
+# this file contains driver code intended to speed up development
+if Rails.env == 'development'
+ # Dir[File.join(Rails.root, "lib", "api", "*.rb")].each do |file|
+  #   require file
+ # end
+  # All apis are required in development, please enter your driver code below
+end
