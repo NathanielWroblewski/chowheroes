fcfa207957f1230f2ab1c0a58c4ab3fd7564a847
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-07-02 00:50:58 -0700

    added tunnels

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   has_many :donations
 
-  validates :email,    :uniqueness => true, :format     => { :with => /@/ }
-  validates :username, :uniqueness => true, :presence   => true
+  validates :email,    :format     => { :with => /@/ }, :on => :create
+  validates :username, :presence   => true,             :uniqueness => true
+  validates :email,    :uniqueness => true
 
   has_secure_password
 end
